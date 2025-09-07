1c689078e9e993f8777c8d4a24f525918a42a845
Nitya Mirle <nmirle25@gmail.com>
2025-09-07 18:34:32 -0500

    Added output match count to specific file

--- find_homologs.sh
+++ find_homologs.sh
@@ -6,3 +6,4 @@ output=$3
 
 tblastn -query "$queryFile" -subject "$subjectFile" -outfmt '6 qseqid sseqid pident length qlen' \ | awk '$3 > 30 && $4 > 0.9 * $5' > "$output"
 
+numMatches=$(wc -l < "$output")
