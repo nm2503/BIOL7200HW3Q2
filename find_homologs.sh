7bc16e4d08a00643733c07645ff6cd7fbda1a905
Nitya Mirle <nmirle25@gmail.com>
2025-09-07 18:32:15 -0500

    Edit output file

--- find_homologs.sh
+++ find_homologs.sh
@@ -2,7 +2,6 @@
 
 queryFile=$1
 subjectFile=$2
-
 output=$3
 
 tblastn -query "$queryFile" -subject "$subjectFile" -outfmt '6 qseqid sseqid pident length qlen' \ | awk '$3 > 30 && $4 > 0.9 * $5' > "$output"
